af23b6271feb77d46255dcdb7138967ffe5fadc2
Alex Soto <alexsotodev@gmail.com>
2022-04-26 04:03:34 -0500

    Created calculate area functions for rectangular/square and circular pans
    May need enum types to switch formulas depending on user input but for now created basic functions for calculating area of pan for rectangular or square and circle

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,37 @@
 use std::io;
 
-fn main() {
-    println!("What is your name?");
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    println!("Hello, {}!", input.trim());
+fn main() 
+{
+    // println!("What is your name?");
+    // let mut input = String::new();
+    // io::stdin().read_line(&mut input).unwrap();
+    // println!("Hello, {}!", input.trim());
+    println!("Area: {}", calculate_area_pan_circle(8.0));
 
+}
+
+fn calculate_area_pan_rect(pan_width: f32, pan_length: f32)->f32
+{
+    let area:f32 = pan_width * pan_length;
+    return area;
+
+}
+
+fn calculate_area_pan_square(pan_size: f32)->f32
+{
+    return calculate_area_pan_rect(pan_size, pan_size);
+}
+
+fn calculate_area_pan_circle(pan_diameter: f32)->f32
+{
+    let area:f32 = std::f32::consts::PI * (pan_diameter/2.0).powi(2);
+    return area;
+}
+
+enum PanType
+{
+    CIRCLE,
+    OVAL,
+    RECT,
+    SQUARE,
 }
