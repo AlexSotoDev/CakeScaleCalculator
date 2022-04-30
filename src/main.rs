509173133647da935650197f7854632a5b5c0a8e
Alex Soto <alexsotodev@gmail.com>
2022-04-30 04:39:19 -0500

    Updated main with my own text message
    Just testing out macroquad text and window with quick example

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::io;
-use macroquad::prelude::*;
+//use std::io;
+use macroquad::{prelude::{*, scene::clear}, color};
 // fn main() 
 // {
 //     // println!("What is your name?");
@@ -12,16 +12,24 @@ use macroquad::prelude::*;
 
 
 #[macroquad::main("BasicShapes")]
-async fn main() {
-    loop {
+async fn main() 
+{
+    // loop {
+    //     clear_background(RED);
+
+    //     draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
+    //     draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
+    //     draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+    //     draw_text("HELLO", 20.0, 20.0, 20.0, DARKGRAY);
+
+    //     next_frame().await
+    // }
+    loop 
+    {
         clear_background(RED);
+        draw_text("hello", 100.0, 100.0, 100.0, color::WHITE);
+        next_frame().await;
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
-        draw_text("HELLO", 20.0, 20.0, 20.0, DARKGRAY);
-
-        next_frame().await
     }
 }
 
